42b1c9b8bd5743e37f5272e9bf482ec45b0f76b2
Bond007 <dedektiga672@gmail.com>
2026-01-20 15:17:11 +0800

    third commit

--- W-2/play.php
+++ W-2/play.php
@@ -2,4 +2,7 @@
 $name = "Qilah";
 echo "Hello World $name ";
 echo 'This is a full stack class'.$name;
+echo "<h1>My name is Faradila. </h1>";
+echo "<h2>My PA name is Miss Athirah. </h2>";
+echo "<h3>My Jabatan is Teknologi Maklumat. </h3>";
 ?>
