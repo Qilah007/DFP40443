98c4c069af1dad74a041c99968460c516d267173
Bond007 <dedektiga672@gmail.com>
2026-01-22 12:13:29 +0800

    better convert-kelvin

--- convert-kelvin.php
+++ convert-kelvin.php
@@ -9,11 +9,21 @@
     </body>
 
     <?php
+        $celsius=null;
         if($_SERVER['REQUEST_METHOD'] == "POST"){
         $kelvin =$_POST['kelvinVal'];
 
         $celsius=$kelvin - 273.15;
         }
+    
+        if ($celcius > 100) {
+            echo "very hot today"
+        } elseif($celcius<50){
+            echo "it's mild today"
+        } else{
+            echo "it's cold today"
+        }
+        
     ?>
 
     <?php
